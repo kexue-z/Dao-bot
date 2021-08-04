7d7688ec4df3106c42948dc61593290bc719cb19
kexue <xana278@foxmail.com>
2021-08-04 19:35:27 +0800

    更新了帮助

--- plugins/help/__init__.py
+++ plugins/help/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_command
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import MessageEvent, Message
 
-message = "[CQ:share,url=https://www.notion.so/kexue/bot-cded534e8aa3434298e63399d6952751,title=屑岛风bot使用指南]"
+message = "[CQ:share,url=https://wiki.kexue.io:3000/,title=屑岛风bot使用指南]"
 
 
 help = on_command('帮助',
