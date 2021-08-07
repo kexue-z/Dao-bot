6d3bf8dee407e58497a6c6b63ef72c8fa22dfaae
kexue <xana278@foxmail.com>
2021-08-08 00:04:24 +0800

    你不会百度吗？

---
+++ plugins/search/__init__.py
@@ -0,0 +1,36 @@
+from .baidu import *
+# from nonebot import on_command
+# from nonebot.adapters.cqhttp import Bot, MessageEvent, MessageSegment
+
+# baidu_url = 'https://www.baidu.com/s?ie=utf-8&wd='
+# bing_url = 'https://www.bing.com/search?q='
+# sogou_url = 'https://www.sogou.com/web?query='
+# google_url = 'https://www.google.com/search?q='
+
+# baidu = on_command('百度', aliases={'baidu'}, priority=1)
+# bing = on_command('必应', aliases={'bing'}, priority=1)
+# sogou = on_command('搜狗', aliases={'sogou'}, priority=1)
+# google = on_command('谷歌', aliases={'google'}, priority=1)
+
+
+
+# @bing.handle()
+# async def _bing(bing: Bot, event: MessageEvent):
+#     msg = bing_url + event.message
+#     await bing.send(message= MessageSegment.share(
+#         url = msg, 
+#         title = str(event.message)))
+
+# @sogou.handle()
+# async def _sogou(sogou: Bot, event: MessageEvent):
+#     msg = sogou_url + event.message
+#     await sogou.send(message= MessageSegment.share(
+#         url = msg, 
+#         title = str(event.message)))
+
+# @google.handle()
+# async def _google(google: Bot, event: MessageEvent):
+#     msg = google_url + event.message
+#     await google.send(message= MessageSegment.share(
+#         url = msg, 
+#         title = str(event.message)))
---
+++ plugins/search/baidu.py
@@ -0,0 +1,13 @@
+from nonebot import on_command
+from nonebot.adapters.cqhttp import Bot, MessageEvent, MessageSegment
+from urllib.parse import quote
+
+baidu_url = 'http://buhuibaidu.me/?s='
+
+baidu = on_command('百度', aliases={'baidu'}, priority=1)
+
+@baidu.handle()
+async def _baidu(bot: Bot, event: MessageEvent):
+    url = baidu_url + quote(str(event.message))
+    # url.replace('&', '&amp;')
+    await baidu.send(message = MessageSegment(type= 'share', data={'url': url, 'title': f'百度{str(event.message)}'}))
