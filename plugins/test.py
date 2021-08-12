ea45fdc672825136928d694ca76c673f6d845928
kexue <xana278@foxmail.com>
2021-08-12 23:11:27 +0800

    📦添加：test

---
+++ plugins/test.py
@@ -0,0 +1,11 @@
+from nonebot import export, on_keyword, on_command
+from nonebot.rule import to_me
+from nonebot.typing import T_State
+from nonebot.permission import SUPERUSER
+from nonebot.adapters.cqhttp import Bot, Event, MessageSegment
+
+test = on_command('kexue', rule= to_me())
+
+@test.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+    await test.finish(message=MessageSegment.image(file =f'file:////test/test.jpg'))
