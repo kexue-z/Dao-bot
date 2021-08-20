9a122847a7eb902b2582daf22f096f89c1ea8e85
kexue <xana278@foxmail.com>
2021-08-20 21:48:53 +0800

    ✨ 引入一些新功能

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -7,7 +7,7 @@ from utils.utils import get_message_text, get_message_imgs
 from nonebot.log import logger
 from nonebot.adapters.cqhttp.message import MessageSegment
 # from models.group_remind import GroupRemind
-# from utils.message_builder import image
+from utils.message_builder import image
 
 __plugin_name__ = "广播 [Hidden]"
 
@@ -39,44 +39,3 @@ async def _(bot: Bot, event: Event, state: T_State):
             except Exception as e:
                 logger.critical(f'向广播发起者进行错误回报时发生错误：{type(e)}')
     await broadcast.send(f'广播完成！')
-
-
-
-def image(
-    img_name: str = None, path: str = None, abspath: str = None, b64: str = None
-) -> MessageSegment or str:
-    """
-    说明：
-        生成一个 MessageSegment.image 消息
-        生成顺序：绝对路径(abspath) > base64(b64) > img_name
-    参数：
-        :param img_name: 图片文件名称，默认在 resource/img 目录下
-        :param path: 图片所在路径，默认在 resource/img 目录下
-        :param abspath: 图片绝对路径
-        :param b64: 图片base64
-    """
-    if abspath:
-        return (
-            MessageSegment.image("file:///" + abspath)
-            if os.path.exists(abspath)
-            else ""
-        )
-    elif b64:
-        return MessageSegment.image(b64 if "base64://" in b64 else "base64://" + b64)
-    else:
-        if "http" in img_name:
-            return MessageSegment.image(img_name)
-        if len(img_name.split(".")) == 1:
-            img_name += ".jpg"
-        file = (
-            Path(IMAGE_PATH) / path / img_name if path else Path(IMAGE_PATH) / img_name
-        )
-        if file.exists():
-            return MessageSegment.image(f"file:///{file.absolute()}")
-        else:
-            logger.warning(f"图片 {file.absolute()}缺失...")
-            return ""
-
-
-
-
