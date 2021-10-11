0f48953fc89d59c584fd2a1ef0bcf83a5234019f
kexue <xana278@qq.com>
2021-10-11 22:11:59 +0800

    🚑 网站修改

--- plugins/help/__init__.py
+++ plugins/help/__init__.py
@@ -2,7 +2,7 @@ from nonebot import on_command
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import MessageEvent, Message
 
-message = "[CQ:share,url=https://wiki.kexue.io/,title=屑岛风bot使用指南]"
+message = "[CQ:share,url=https://wiki.kexue.io:3000/,title=屑岛风bot使用指南]"
 
 
 help = on_command("帮助", aliases={"机器人帮助", "机器人说明"}, priority=1)
