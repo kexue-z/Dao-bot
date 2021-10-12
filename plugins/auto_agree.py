7582b9a20f50acbf288ab16d0758c8adf0bc9c64
kexue <xana278@foxmail.com>
2021-10-12 19:59:37 +0800

    ✨ 添加好友/群

--- plugins/auto_agree.py
+++ plugins/auto_agree.py
@@ -9,20 +9,21 @@ super_req = on_request(priority=5)
 
 @super_req.handle()
 async def add_superuser(bot: Bot, event: RequestEvent, state: T_State):
+    admin = int(list(bot.config.superusers)[0])
 
     if str(event.user_id) in bot.config.superusers:
         if event.request_type == "private":
             await event.approve(bot)
-            await event.send(f"添加好友{event.user_id}")
+            await bot.send_private_msg(message=f"添加好友{event.user_id}", user_id=admin)
 
         elif event.sub_type == "invite" and event.request_type == "group":
             await event.approve(bot)
-            await event.send(f"添加群{event.group_id}")
+            await bot.send_private_msg(message=f"添加群{event.group_id}", user_id=admin)
 
     # elif event.request_type == "invite":
     else:
         # if
-        user_name = bot.get_stranger_info(event.user_id)
+        user_name = bot.get_stranger_info(user_id=event.user_id, no_cache=True)
         msg = (
             f"[收到{'好友' if event.request_type == 'friend' else '加群'}邀请]\n"
             f"邀请人: {user_name}({event.user_id})\n"
@@ -44,9 +45,9 @@ approve_friend = on_command("添加好友", permission=SUPERUSER)
 @approve_friend.handle()
 async def _(bot: Bot, event: MessageEvent):
     flag = str(event.get_message())
-    await bot.set_friend_add_request(flag=flag,approve=True)
+    await bot.set_friend_add_request(flag=flag, approve=True)
     admin = int(list(bot.config.superusers)[0])
-    await bot.send_private_msg(message="已添加好友",user_id=admin)
+    await bot.send_private_msg(message="已添加好友", user_id=admin)
 
 
 approve_group = on_command("添加群", permission=SUPERUSER)
@@ -57,4 +58,4 @@ async def _(bot: Bot, event: MessageEvent):
     flag = str(event.get_message())
     await bot.set_group_add_request(flag=flag, sub_type="invite", approve=True)
     admin = int(list(bot.config.superusers)[0])
-    await bot.send_private_msg(message="已添加群",user_id=admin)
+    await bot.send_private_msg(message="已添加群", user_id=admin)
