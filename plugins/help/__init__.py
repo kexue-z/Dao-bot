a6bed758920283d9af6588138435f5f348596106
kexue <xana278@foxmail.com>
2021-10-29 11:56:07 +0800

    🐛 修复正则语法错误bug

--- plugins/help/__init__.py
+++ plugins/help/__init__.py
@@ -5,7 +5,7 @@ from nonebot.adapters.cqhttp.event import MessageEvent, Message
 message = "[CQ:share,url=https://wiki.kexue.io:3000/,title=屑岛风bot使用指南]"
 
 
-help = on_regex(r"(帮助)|(机器人帮助)|(机器人说明)",priority=1)
+help = on_regex(r"^(帮助|机器人帮助|机器人说明)$",priority=1)
 
 
 @help.handle()
