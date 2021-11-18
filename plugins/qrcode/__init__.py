b59cdc62f92f298dbc5eef348f024c43eaeb2c1b
kexue <xana278@qq.com>
2021-11-18 11:50:17 +0800

    ✨ 二维码识别

--- plugins/qrcode/__init__.py
+++ plugins/qrcode/__init__.py
@@ -3,50 +3,86 @@ from typing import Dict
 
 from httpx import AsyncClient
 from nonebot import on_command, on_message
-from nonebot.adapters.cqhttp import (Bot, GroupMessageEvent, Message,
-                                     MessageEvent, PrivateMessageEvent)
+from nonebot.adapters.cqhttp import (
+    Bot,
+    GroupMessageEvent,
+    Message,
+    MessageEvent,
+    PrivateMessageEvent,
+)
 from nonebot.typing import T_State
 from PIL import Image
 from pyzbar.pyzbar import decode
 from nonebot.log import logger
+
 # from .qr_handle import get_qr_data
 
-# qr_map: Dict[str, str] = {}  # 保存这个群的上一张色图 {"123456":"http://xxx"}
+qr_map: Dict[str, str] = {}
 
 
-# async def check_qrcode(bot: Bot, event: MessageEvent, state: T_State) -> bool:
-#     if isinstance(event, MessageEvent):
-#         for msg in event.message:
-#             if msg.type == "image":
-#                 url: str = msg.data["url"]
-#                 state["url"] = url
-#                 return True
-#         return False
+async def check_qrcode(bot: Bot, event: MessageEvent, state: T_State) -> bool:
+    if isinstance(event, MessageEvent):
+        for msg in event.message:
+            if msg.type == "image":
+                url: str = msg.data["url"]
+                state["url"] = url
+                return True
+        return False
 
 
-# notice_qrcode = on_message(check_qrcode)
+notice_qrcode = on_message(check_qrcode)
 
 
-# @notice_qrcode.handle()
-# async def handle_pic(bot: Bot, event: GroupMessageEvent, state: T_State):
-#     try:
-#         group_id: str = str(event.group_id)
-#         qr_map.update({group_id: state["url"]})
-#     except AttributeError:
-#         pass
-    
+@notice_qrcode.handle()
+async def handle_pic(bot: Bot, event: MessageEvent, state: T_State):
+    if isinstance(event, GroupMessageEvent):
+        try:
+            group_id: str = str(event.group_id)
+            qr_map.update({group_id: state["url"]})
+        except AttributeError:
+            pass
+    elif isinstance(event, PrivateMessageEvent):
+        try:
+            user_id: str = str(event.user_id)
+            qr_map.update({user_id: state["url"]})
+        except ArithmeticError:
+            pass
 
-# qrcode = on_command("上一张二维码", aliases={"qrcode"})
+
+pqr = on_command("pqr", aliases={"前一二维码", "pqrcode"})
 
 
-qrcode = on_command("qrcode",aliases={"qr","二维码"})
+@pqr.handle()
+async def handle_pqr(bot: Bot, event: MessageEvent, state: T_State):
+    try:
+        url: str = (
+            qr_map[str(event.group_id)]
+            if isinstance(event, GroupMessageEvent)
+            else qr_map[str(event.user_id)]
+        )
+
+        async with AsyncClient() as client:
+            res = await client.get(url=url, timeout=10)
+        img = Image.open(BytesIO(res.content))
+        data = decode(img)
+        qr_data = data[0][0]
+        await pqr.finish(str(qr_data.decode()))
+    except (IndexError):
+        await pqr.finish()
+    except KeyError:
+        await pqr.finish("图不对！")
+
+
+qrcode = on_command("qrcode", aliases={"qr", "二维码"})
+
 
 @qrcode.handle()
 async def handle_first_receive(bot: Bot, event: MessageEvent, state: T_State):
     msg = event.message
     if msg:
-        state["setu"] = msg
+        state["qr_img"] = msg
     pass
+
 
 @qrcode.got("qr_img", prompt="图呢")
 async def get_qr_img(bot: Bot, event: MessageEvent, state: T_State):
@@ -54,12 +90,13 @@ async def get_qr_img(bot: Bot, event: MessageEvent, state: T_State):
     # try:
     if msg[0].type == "image":
         url = msg[0].data["url"]
-        
+
         async with AsyncClient() as client:
             res = await client.get(url=url, timeout=10)
-        img= Image.open(BytesIO(res.content))
+        img = Image.open(BytesIO(res.content))
         data = decode(img)
         qr_data = data[0][0]
         logger.info(str(qr_data.decode()))
         await qrcode.finish(str(qr_data.decode()))
-
+    else:
+        await qrcode.finish("这啥？指令已取消")
