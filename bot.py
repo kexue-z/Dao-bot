a0a66498b92ae22f511d5f4bc8df54ba0cc744af
kexue <xana278@qq.com>
2022-01-18 09:36:37 +0800

    🚀

--- bot.py
+++ bot.py
@@ -12,18 +12,8 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", CQHTTPBot)
 
-# nonebot.load_plugin("nonebot_plugin_htmlrander")
-nonebot.load_plugins("plugins")
-nonebot.load_plugin("nonebot_plugin_cocdicer")
-nonebot.load_plugin("nonebot_plugin_manager")
-nonebot.load_plugin("nonebot_bison")
-nonebot.load_plugin("nonebot_plugin_tvseries")
-nonebot.load_plugin("nonebot_plugin_heweather")
-nonebot.load_plugin("nonebot_plugin_nokia")
-# nonebot.load_plugin("nonebot_plugin_setu_now")
-nonebot.load_plugin("nonebot_plugin_picsearcher")
-nonebot.load_plugin("nonebot_plugin_trpglogger")
-nonebot.load_plugin("nonebot_plugin_status")
+
+nonebot.load_from_toml("pyproject.toml")
 
 if __name__ == "__main__":
     nonebot.run(app="bot:app")
