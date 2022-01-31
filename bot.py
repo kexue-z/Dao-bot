0e0d989a1bd550a6ba8240ef55963c84a0cfa353
kexue <xana278@qq.com>
2022-01-31 11:50:37 +0800

    🐛 fix

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+
 import nonebot
-from nonebot.adapters.onebot.v11 import Adapter
-from utils.yaml import Secrets, load_yaml
 from nonebot.log import logger
+from nonebot.adapters.onebot.v11 import Adapter
 
-logger.add("data/log/nonebotlog.log",rotation="5 MB")
+from utils.yaml import Secrets, load_yaml
 
-config = load_yaml("config/config.yaml", Secrets("config"))
-nonebot.init(**config)
+logger.add("data/log/nonebotlog.log", rotation="5 MB")
+
+config_json = load_yaml("config/config.yaml", Secrets(Path("config")))
+nonebot.init(**dict(config_json))
 
 
 app = nonebot.get_asgi()
@@ -18,7 +21,9 @@ driver = nonebot.get_driver()
 driver.register_adapter(Adapter)
 
 
-nonebot.load_from_toml("pyproject.toml")
+all_plugins = load_yaml("config/plugins.yaml")
+nonebot.load_all_plugins(all_plugins["plugins"], all_plugins["plugin_dirs"])
+
 
 if __name__ == "__main__":
     nonebot.run()
