8a29a478e378404c3cb235bb2f856821d747dfef
kexue <x@kexue.io>
2022-05-03 17:23:47 +0800

    ✨ 添加自我更新

---
+++ plugins/self_update.py
@@ -0,0 +1,30 @@
+from httpx import AsyncClient
+from nonebot import get_driver, on_command
+from pydantic import Extra, BaseModel
+from nonebot.log import logger
+from nonebot.permission import SUPERUSER
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    github_api_key: str
+
+
+API_KEY = Config.parse_obj(get_driver().config.dict()).github_api_key
+API_URL = "https://api.github.com/repos/kexue-z/Dao-bot/actions/workflows/auto-update.yml/dispatches"
+
+
+auto_update = on_command("自我更新", permission=SUPERUSER, block=True, priority=1)
+
+
+@auto_update.handle()
+async def _():
+    headers = {
+        "Accept": "application/vnd.github.v3+json",
+        "Authorization": f"token {API_KEY}",
+    }
+    data = {"ref": "master"}
+    async with AsyncClient() as client:
+        res = await client.post(API_URL, headers=headers, json=data)
+        if res.status_code == 204:
+            logger.success("已发起自我更新请求")
+        await auto_update.finish("已发起自我更新请求")
