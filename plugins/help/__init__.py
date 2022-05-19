ddf6d2a474ba27f36c0c74e512ca02a3d7919888
kexue <x@kexue.io>
2022-05-19 15:12:30 +0800

    🤔 修改帮助

--- plugins/help/__init__.py
+++ plugins/help/__init__.py
@@ -1,9 +1,8 @@
 from nonebot import on_command, on_keyword
-from nonebot.adapters.onebot.v11 import Message
-from nonebot.matcher import Matcher
 from nonebot.rule import to_me
+from nonebot.matcher import Matcher
 
-message = "[CQ:share,url=https://kexue-z.github.io/Dao-bot/,title=屑岛风bot使用指南]"
+message = "https://kexue-z.github.io/Dao-bot/"
 
 
 help_cmd = on_command("帮助", aliases={"机器人帮助", "机器人说明"}, priority=1)
@@ -13,4 +12,4 @@ help_msg = on_keyword({"帮助", "help", "机器人帮助", "机器人说明"}, 
 @help_msg.handle()
 @help_cmd.handle()
 async def _(matcher: Matcher):
-    await matcher.finish(message=Message(message))
+    await matcher.finish(message=message)
